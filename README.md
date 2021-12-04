04a553e047a2751bfb4c178307619b07af9665f9
Petar Dosev <pdosev@gmail.com>
2021-12-04 10:37:23 -0500

    Add readme, fix day 2 solution 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2021
+
+This time in Lua! It needs the inspect library in your path somewhere, or just remove the require call.
--- day2/solution2.lua
+++ day2/solution2.lua
@@ -5,14 +5,16 @@ io.input("input.txt")
 input = strings.split(io.read("*all"), "\r\n")
 x = 0
 y = 0
+aim = 0
 for index, command in ipairs(input) do
   instruction = strings.split(command)
   if instruction[1] == "up" then
-    y = y - instruction[2]
+    aim = aim - instruction[2]
   elseif instruction[1] == "down" then
-    y = y + instruction[2]
+    aim = aim + instruction[2]
   elseif instruction[1] == "forward" then
     x = x + instruction[2]
+    y = aim * instruction[2] + y
   end
 end
 print(x * y)
